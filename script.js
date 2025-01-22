d18b89a2eeb8df0aa8cbca988e25284ff550afea
adib <mirzamohid1001@gmail.com>
2025-01-23 00:10:30 +0800

    refine responsive scale

--- script.js
+++ script.js
@@ -3,48 +3,50 @@ const result = document.getElementById('result');
 
 window.addEventListener('scroll', function () {
     const nav = document.querySelector('nav');
-    nav.classList.toggle('sticky', window.scrollY > 0)
+    nav.classList.toggle('shrink-nav', window.scrollY > 50)
 })
-
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const object = Object.fromEntries(formData);
-  const json = JSON.stringify(object);
-  result.innerHTML = "Please wait..."
-
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = "Form submitted successfully";
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function() {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 3000);
-        });
-});
 
 const barsIcon = document.getElementsByClassName('fa-bars');
 const xmark = document.getElementsByClassName('fa-xmark');
 
 function toggleMenu() {
     document.querySelector('.menu-button').classList.toggle('active');
+    document.querySelector('ul').classList.toggle('active');
+    document.querySelector('nav').classList.toggle('shrink-nav')
 }
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+  
+      fetch('https://api.web3forms.com/submit', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+              },
+              body: json
+          })
+          .then(async (response) => {
+              let json = await response.json();
+              if (response.status == 200) {
+                  result.innerHTML = "Form submitted successfully";
+              } else {
+                  console.log(response);
+                  result.innerHTML = json.message;
+              }
+          })
+          .catch(error => {
+              console.log(error);
+              result.innerHTML = "Something went wrong!";
+          })
+          .then(function() {
+              form.reset();
+              setTimeout(() => {
+                  result.style.display = "none";
+              }, 3000);
+          });
+});
